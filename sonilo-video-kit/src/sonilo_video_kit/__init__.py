930cd977cadde434b13d63bcbacd4293c68b7039
Spencer Qian <spencer@sonilo.com>
2026-07-18 02:50:48 -0700

    feat(video-kit): mix_with_video ffmpeg pipeline

--- sonilo-video-kit/src/sonilo_video_kit/__init__.py
+++ sonilo-video-kit/src/sonilo_video_kit/__init__.py
@@ -4,3 +4,6 @@ __all__: list[str] = []
 
 from .generate import VideoMusicClient, generate_music_for_video
 __all__ += ["generate_music_for_video", "VideoMusicClient"]
+
+from .mix import mix_with_video
+__all__ += ["mix_with_video"]
